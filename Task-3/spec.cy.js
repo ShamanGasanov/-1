463e2f1ca89966f58921cf8f11a0aa1c030a45ed
Shamil Gasanov <133899024+ShamanGasanov@users.noreply.github.com>
2024-10-12 13:20:35 +0400

    Add files via upload

---
+++ Task-3/spec.cy.js
@@ -0,0 +1,66 @@
+describe('template spec', () => {
+  beforeEach(() =>{
+    cy.visit ('https://www.saucedemo.com')
+  }
+)
+  it('вводит данные в поля логина и пароля', () => {
+    // Переход на страницу
+    cy.visit('https://www.saucedemo.com');
+
+    // Ввод логина
+    cy.get('input[name="user-name"]').type('standard_user');
+
+    // Ввод пароля
+    cy.get('input[name="password"]').type('secret_sauce');
+
+    // Отправка формы
+    cy.get('input[type="submit"]').click();
+  });
+
+  it('сортировка', () => {
+    cy.get('input[name="user-name"]').type('standard_user');
+
+    // Ввод пароля
+    cy.get('input[name="password"]').type('secret_sauce');
+
+    // Отправка формы
+    cy.get('input[type="submit"]').click();
+    // Убедитесь, что страница загружена
+    cy.wait(1000); // Задержка на 1 секунду
+
+    // Смена сортировки
+    cy.get('.product_sort_container').select('lohi');
+  });
+
+  it('Нажатие на кнопки добавления в корзину двух товаров и оформление заказа', () => {
+    // Убедитесь, что страница загружена
+    cy.get('input[name="user-name"]').type('standard_user');
+
+    // Ввод пароля
+    cy.get('input[name="password"]').type('secret_sauce');
+
+    // Отправка формы
+    cy.get('input[type="submit"]').click();
+    cy.wait(1000); // Задержка на 1 секунду
+
+    // Нажатие на кнопки добавления в корзину
+    cy.get('.btn.btn_primary.btn_small.btn_inventory').eq(0).click();
+    cy.get('.btn.btn_primary.btn_small.btn_inventory').eq(1).click();
+
+    // Переход к корзине
+    cy.scrollTo('top');
+    cy.get('.shopping_cart_link').click();
+
+    // Оформление заказа
+    cy.get('.checkout_button').click();
+    cy.get('input[name="firstName"]').type('standard_user');
+    cy.get('input[name="lastName"]').type('standard_user');
+    cy.get('input[name="postalCode"]').type('1337');
+    cy.get('.btn_action').click();
+
+    // Возврат в корзину
+    cy.scrollTo('bottom');
+    cy.get('.cart_button').click();
+  });
+});
+
